474bd80e97d46579bce3614d2c310bef978a39e5
billal-webdev <billal-webdev@gmail.com>
2023-11-29 09:44:39 +0600

    added activity log to member's dashboard activity log page

--- src/pages/Dashboard/Members/ActivityLog/ActivityLog.jsx
+++ src/pages/Dashboard/Members/ActivityLog/ActivityLog.jsx
@@ -1,10 +1,45 @@
 import React from 'react'
 import useTitle from '../../../../hooks/useTitle'
+import useLoadData from '../../../../hooks/useLoadData';
+import useAuth from '../../../../hooks/useAuth';
+import Spinner from '../../../../components/Spinner';
 
 const ActivityLog = () => {
   useTitle("FitnessHub | Activity Log")
+  const {user} = useAuth();
+  const {loading, error, data:bookedTrainer} = useLoadData(`booked-trainers?memberEmail=${user?.email}`);
+
+  const {trainerName, trainerEmail, bookedSlot, classes} = bookedTrainer || {};
   return (
-    <div>ActivityLog</div>
+    <div>
+      <h3 className='text-2xl font-medium my-3'>Today's Activity</h3>
+   {
+    loading ? <Spinner></Spinner> : <div className="overflow-x-auto">
+    <table className="table">
+      {/* head */}
+      <thead className='bg-[#707e2c] text-white'>
+        <tr>
+          <th></th>
+          <th>Trainer</th>
+          <th>Trainer Email</th>
+          <th>Class</th>
+          <th>Time</th>
+        </tr>
+      </thead>
+      <tbody>
+        {/* row */}
+        <tr>
+          <th>1</th>
+          <td>{trainerName}</td>
+          <td>{trainerEmail}</td>
+          <td>{classes && classes[0]}</td>
+          <td>{bookedSlot}</td>
+        </tr>
+      </tbody>
+    </table>
+    </div>
+   }
+    </div>
   )
 }
 
--- src/pages/Dashboard/Members/RecommendedClasses/RecommendedClasses.jsx
+++ src/pages/Dashboard/Members/RecommendedClasses/RecommendedClasses.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import useTitle from '../../../../hooks/useTitle'
 import useLoadData from '../../../../hooks/useLoadData'
 import ClassCard from './ClassCard'
+import Spinner from '../../../../components/Spinner'
 
 const RecommendedClasses = () => {
   useTitle("FitnessHub | Recommended Classes")
@@ -10,11 +11,13 @@ const RecommendedClasses = () => {
   return (
     <div>
       <h3 className="font-bold md:text-3xl text-3xl mt-10 mb-5 ml-3">Recommended Classes</h3>
-      <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5 px-3">
-         {
-          classes?.slice(4).map((classItem)=> <ClassCard key={classItem._id} classItem={classItem}></ClassCard>)
-         }
-      </div>
+      {
+        loading ? <Spinner></Spinner> : <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5 px-3">
+        {
+         classes?.slice(4).map((classItem)=> <ClassCard key={classItem._id} classItem={classItem}></ClassCard>)
+        }
+     </div>
+      }
     </div>
   )
 }
